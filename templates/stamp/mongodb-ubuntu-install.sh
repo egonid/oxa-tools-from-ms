e68bf708c69af8a897a8de3bf94a05f8185f535e
egonid <egonid@outlook.com>
2019-09-09 10:26:23 +0900

    Update mongodb-ubuntu-install.sh

--- templates/stamp/mongodb-ubuntu-install.sh
+++ templates/stamp/mongodb-ubuntu-install.sh
@@ -137,16 +137,16 @@ install_mongodb()
     log "Downloading MongoDB package $PACKAGE_NAME from $PACKAGE_URL"
 
     # Configure mongodb.list file with the correct location
-    #if (( $(echo "$OS_VER > 16" | bc -l) ))
-    #then
-     #   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-     # echo "deb ${PACKAGE_URL} "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-    
     if (( $(echo "$OS_VER > 16" | bc -l) ))
     then
-    curl -LO https://www.mongodb.org/static/pgp/server-3.2.asc
-    gpg --import server-3.2.asc
-    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+       apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 614d985504a2163b
+     echo "deb ${PACKAGE_URL} "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+    
+    #if (( $(echo "$OS_VER > 16" | bc -l) ))
+    #then
+    #curl -LO https://www.mongodb.org/static/pgp/server-3.2.asc
+    #gpg --import server-3.2.asc
+    #echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
         
     else
         apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
